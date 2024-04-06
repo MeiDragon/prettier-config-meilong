c599f838fd0954b20ab3ab1a5e9b5e91c9f45477
HduSy <hdusy14032122@gmail.com>
2024-04-06 20:39:26 +0800

    feat: Prettier 共享配置

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+}
